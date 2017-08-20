b2f2d313dddc90ccd32bf83357287e39bf563771
b3n10 <francisben.lleve@gmail.com>
2017-08-20 15:45:31 +0800

    up to footer

--- index.php
+++ index.php
@@ -22,7 +22,46 @@
 					<li><a href="#">contact me</a></li>
 				</ul>
 			</nav>
+		</div><!--end of header-->
+		<div class="main">
+			<h1>welcome to my page</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div><!--end of main -->
+		<hr>
+		<div class="section">
+			<ul>
+				<li>
+					<h3>
+						<a href="#">item 1</a>
+					</h3>
+					<a href="#">
+						<img src="#" alt="#">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+					</a>
+				</li>
+				<li>
+					<h3>
+						<a href="#">item 1</a>
+					</h3>
+					<a href="#">
+						<img src="#" alt="#">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+					</a>
+				</li>
+				<li>
+					<h3>
+						<a href="#">item 1</a>
+					</h3>
+					<a href="#">
+						<img src="#" alt="#">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+					</a>
+				</li>
+			</ul>
+		</div><!--end of section-->
+		<div class="footer">
+			<p>Ben Lleve &copy; 2017</p>
 		</div>
-	</div>
+	</div><!-- end of page -->
 </body>
 </html>
